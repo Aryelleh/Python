7ebbe0dbf622c28b693c3a1da910e29d441cdfbc
Aryelleh <carolinafreitas1999@gmail.com>
2023-04-23 13:35:36 +0200

    edited comments

--- codewars/kata2.py
+++ codewars/kata2.py
@@ -1,27 +1,28 @@
 def duplicate_count(passed_string):
     if not passed_string or not any(char.isalnum() for char in passed_string):
+        # returns 0 if string is empty
         return 0                      
-    # returns 0 if string is empty
     
     char_count = {}
     for char in passed_string.lower():
         if not char.isalnum():
+            # returns 0 for every non-alphanumeric character
             return 0                  
-        # returns 0 for every non-alphanumeric character
-        
+             
         if char in char_count:
             char_count[char] += 1
         else:
-            char_count[char] = 1
-        # everytime a character from the char_count dictionary appears in a string, increase count by one
+            # everytime a character from the char_count dictionary appears in a string, increase count by one
+            char_count[char] = 1   
 
     duplicate_count = {}
     for char, count in char_count.items():
         if count != 1:
+            # if any characters in the string have a count greater than 1, display count
             duplicate_count[char] = count
-    # if any characters in the string have a count greater than 1, display count
 
-    return len(duplicate_count) # returns the amount of characters that repeat
+    # returns the amount of characters that repeat
+    return len(duplicate_count) 
 
 test1_string = "Excellent"
 test2_string = "Pokemon"
